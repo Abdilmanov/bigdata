82ecef24962836ee918620b052bd45e6b8a5da1f
YeJa <abdilmanovyernar@gmail.com>
2019-07-16 17:06:58 +0600

    check

--- js/scriptOLD.js
+++ js/scriptOLD.js
@@ -85,7 +85,7 @@ require([
 
     let typeHomeElement = document.querySelector('[aria-label="Отобразить типы зданий"]');
     let graphicElement = document.querySelector('[aria-label="Узнать данные по области"]');
-
+    console.log(typeHomeElement);
     typeHomeElement.addEventListener('click', function() {
       console.log(1);
       if (geometryDrawExpand.expanded = true) geometryDrawExpand.expanded = false;
